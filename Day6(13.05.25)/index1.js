47a9582e88964558daccfa3a02feac5da058b334
Rohit Singh <rs21Rohit@gmail.com>
2025-05-14 16:42:34 +0530

    .

--- Day6(13.05.25)/index1.js
+++ Day6(13.05.25)/index1.js
@@ -1,25 +1,28 @@
-const user = {
-    id1:{
-        username:"Rohit",
-        password:"Rohit",
-        role:"User"
-    },
+// const user = {
+//     id1:{
+//         username:"Rohit",
+//         password:"Rohit",
+//         role:"User"
+//     },
     
-    id2:{
-        username:"Amit",
-        password:"Amit",
-        role:"Employee"
-    },
-    id3:{  
-        username:"Aniket",
-        password:"Aniket",
-        role:"Admin"
-    }
-}
+    // id2:{
+    //     username:"Amit",
+    //     password:"Amit",
+    //     role:"Employee"
+    // },
+    // id3:{  
+    //     username:"Aniket",
+    //     password:"Aniket",
+    //     role:"Admin"
+    // }
+// }
 // console.log(user)
 // const {user1:{username,password,role},user2:{},user3:{}} = user
 // // const {name1,age} = user[1]
 // console.log(user1.username)
 // const {username} = user.id1
-const{id1,id2,id3} = user;
-console.log(id1.username)
+let user = {
+    a:"hi"
+}
+const{b} = user;
+console.log(b)
