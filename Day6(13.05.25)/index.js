76c2a55b7e27ac8c3105a2bcfcad8753289f2b0f
Rohit Singh <rs21Rohit@gmail.com>
2025-05-14 16:16:58 +0530

    how to select id

--- Day6(13.05.25)/index.js
+++ Day6(13.05.25)/index.js
@@ -65,8 +65,7 @@ const user = {
         role:"Admin"
     }
 }
-const {id1,id2,id3} = user
-
+const {idd1,idd2,idd3} = user
 
 // const authRoute =(req,res,next)=>{
 //     if(req.body.user.id.username === user.id.username && req.body.user.id.username === user.id.password){
@@ -77,11 +76,19 @@ const {id1,id2,id3} = user
 //         res.send("Username or password does not match")
 //     }
 // }
+
 const userRoute = (req,res,next)=>{
-    if(req.body.id1.username == id1.username && req.body.id1.password == id1.password){
-        res.send("User and password or correct");
-        if(req.body.id1.role === id1.role){
-            res.send("Role is correct")
+    const {id1} = req.body;
+    // res.send("Entered userroute")
+    
+    // res.send(id1.username)
+    // res.send(password)
+    // res.send(role)
+    if(id1.username === idd1.username && req.body.id1.password === idd1.password){
+        // res.send("User and password or correct");
+        if(id1.role === idd1.role){
+            // res.send("Role is correct")
+            
             next()
         }
         else{
@@ -94,38 +101,60 @@ const userRoute = (req,res,next)=>{
 
 }
 const adminRoute = (req,res,next)=>{
-    if(req.body.id3.role == id3.role){
-        res.send("yes")
+    const {id3} = req.body;
+     if(id3.username === idd3.username && req.body.id3.password === idd3.password){
+   
+        if(idd3.role == idd3.role){
+
+        // res.send("yes")
         next()
+        
+     } 
+     else{
+         res.send("does not match")
+        }
     }
     else{
-        res.send("does not match")
+        res.send("user not found")
     }
+
 
 }
 
 const EmployeeRoute = (req,res,next)=>{
-    if(req.user.id2.role === id2.role){
-        res.send("yes")
+    const {id2} = req.body;
+     if(id2.username === idd2.username && req.body.id2.password === idd2.password){
+   
+    if(idd2.role === idd2.role){
+        // res.send("yes")
         next()
+        
     }
     else{
         res.send("does not match")
+        }
     }
-
+    else{
+        res.send("User not found  ")
+    }
 }
-app.post("/user",userRoute,(req,res)=>{
+app.post("/user",userRoute,(req,res,next)=>{
     // userRoute()
+    // next()
     res.send("user route")
+
     
 
 })
-app.post("/admin",adminRoute,(req,res)=>{
+app.post("/admin",adminRoute,(req,res,next)=>{
+    // next()
     res.send("Admin Route")
+
     // adminRoute()
 })
-app.post("/employee",EmployeeRoute,(req,res)=>{
+app.post("/employee",EmployeeRoute,(req,res,next)=>{
     // EmployeeRoute()
+    // next()
     res.send("Employee Route")
 })
 app.get("/a",(req,res)=>{
