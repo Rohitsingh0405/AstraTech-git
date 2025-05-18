8575c6eb7ff4fe0890a7b2274455d901a5f97393
Rohit Singh <rs21Rohit@gmail.com>
2025-05-18 19:06:19 +0530

    Signup endpoint is created

--- practise/Todo-Practise/index.js
+++ practise/Todo-Practise/index.js
@@ -1,15 +1,21 @@
 const express = require("express")
+const bcrypt = require("bcrypt")
 const app = express();
 const user = []
 const fs = require('fs')
 app.post("/Signup",(req,res)=>{
 const {username,password} = req.body;
-const data = user.push({Username:username , Password:password})
-fs.writeFileSync("database.json",data)
-
+const hashPassword = bcrypt.hash(password,10)
+user.push({Username:username , Password:hashPassword})
+fs.writeFileSync("datbase.json",user)
 })
 app.post("/Login",(req,res)=>{
-
+    
+})
+app.post("/",(req,res)=>{
+ if(!fs.existsSync("database.json")){
+    fs.writeFileSync("database.json","Database file \n")
+ }
 })
 app.listen(8080,()=>{
     console.log("Server started")
