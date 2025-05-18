78c4a4f4d9a102d8b97949191fc0ae117a8e7de8
Rohit Singh <rs21Rohit@gmail.com>
2025-05-18 19:22:12 +0530

    .

--- practise/Todo-Practise/index.js
+++ practise/Todo-Practise/index.js
@@ -1,21 +1,32 @@
 const express = require("express")
 const bcrypt = require("bcrypt")
+
 const app = express();
+app.use(express.json())
 const user = []
-const fs = require('fs')
-app.post("/Signup",(req,res)=>{
+const fs = require('fs');
+const { json } = require("stream/consumers");
+app.post("/Signup",async(req,res)=>{
 const {username,password} = req.body;
-const hashPassword = bcrypt.hash(password,10)
+// const hashUsername = await bcrypt.hash(username,10)
+const hashPassword = await bcrypt.hash(password,10)
 user.push({Username:username , Password:hashPassword})
 fs.writeFileSync("datbase.json",user)
 })
 app.post("/Login",(req,res)=>{
+    // const {username,password} = req.body;
+    res.json(user.hashUsername)
     
+
 })
 app.post("/",(req,res)=>{
  if(!fs.existsSync("database.json")){
-    fs.writeFileSync("database.json","Database file \n")
+    fs.writeFileSync("database.json","Database is Ready \n" )
+    res.status(200).json("Database is Ready")
+    return
  }
+ res.status(200).json("database is already created")
+
 })
 app.listen(8080,()=>{
     console.log("Server started")
