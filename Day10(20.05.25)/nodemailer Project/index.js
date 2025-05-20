e868e7ba3e39de76900d09d59c8db6c1be574efa
Rohit Singh <rs21Rohit@gmail.com>
2025-05-20 19:28:03 +0530

    .

---
+++ Day10(20.05.25)/nodemailer Project/index.js
@@ -0,0 +1,25 @@
+const nodemailer = require("nodemailer")
+const tansporter = nodemailer.createTransport({
+    service:'gmail',
+    auth:{
+        user:"rs21rohit@gmail.com",
+        pass:"rgar vjfp kvzq mkmm"
+    }
+})
+
+const sendMailTo = 'rs21rohit@gmail.com';
+const mailoptions = {
+    from:"rs21rohit@gmail.com",
+    to:sendMailTo,
+    subject:"Just testing",
+    text:"I am testing"
+}
+
+tansporter.sendMail(mailoptions,function(err,info){
+    if(err){
+        console.log({Message:err})
+    }
+    else{
+        console.log({message:info.response})
+    }
+})
