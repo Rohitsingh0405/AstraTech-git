f0412eb95355c3283761dababed0b436028e5ac4
Rohit Singh <rs21Rohit@gmail.com>
2025-06-04 19:04:53 +0530

    .

--- Todo-App/index.js
+++ Todo-App/index.js
@@ -8,6 +8,7 @@ const { json } = require('stream/consumers')
 const app = express();
 const cors = require('cors')
 const prisma = require('./prismasetup')
+const { randomUUID } = require('crypto')
 app.use(cors())
 app.use(express.json())
 
@@ -50,26 +51,52 @@ app.get("/getUser",async(req,res)=>{
 
 app.post("/Signup",async(req,res)=>{
 
-const {username,password} = req.body
-console.log("Eror kya hai")
+const {username,password,email} = req.body
 const hashpass =await bcrypt.hash(password,10)
 user.push({username,password:hashpass})
-const readData = fs.readFileSync("database.json",'utf-8')
-const readDataParse = JSON.parse(readData)
-const findUser = readDataParse.find((users)=>users.username == username) //Ek naam ka ek he user hoga 
-console.log(readDataParse)
+const userss = prisma.User.findMany({
+    where:{name:username}
 
- if(findUser){
-    res.status(200).json({Message:`${username} app zinda hai exits krte hai`})
+})
+console.log(userss)
+if(userss){
+    res.json({
+        "DATA":"User already exists"
+    })
     return
 }
+const add = async ()=>{
+    const User =await prisma.User.create({
+        data:{
+            id:randomUUID(),
+            name:username,
+            email:email,
+            password:hashpass
 
-// writeDataFuntion()
-readDataParse.push({username,password:hashpass})
-fs.writeFileSync('database.json',JSON.stringify(readDataParse,null,2))
-// findUser = readDataParse.find((users)=>users.username == username) //Ek naam ka ek he user hoga 
+        }
+    })
+}
+add()
+res.json({
+    data:"User ADDED"
+})
 
-res.json("You are now Signed Up")
+// const readData = fs.readFileSync("database.json",'utf-8')
+// const readDataParse = JSON.parse(readData)
+// const findUser = readDataParse.find((users)=>users.username == username) //Ek naam ka ek he user hoga 
+// console.log(readDataParse)
+
+//  if(findUser){
+//     res.status(200).json({Message:`${username} app zinda hai exits krte hai`})
+//     return
+// }
+
+// // writeDataFuntion()
+// readDataParse.push({username,password:hashpass})
+// fs.writeFileSync('database.json',JSON.stringify(readDataParse,null,2))
+// // findUser = readDataParse.find((users)=>users.username == username) //Ek naam ka ek he user hoga 
+
+// res.json("You are now Signed Up")
 
 })
 app.post("/Login",async(req,res)=>{
